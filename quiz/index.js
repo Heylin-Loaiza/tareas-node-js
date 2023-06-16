cd35bdcff9034ff38dd71ab46a1bf79de6f268e5
Heylin-Loaiza <heylinloaiza.s@gmail.com>
2023-06-15 20:58:00 -0600

    quiz update

--- quiz/index.js
+++ quiz/index.js
@@ -1,11 +1,14 @@
-const infoPokemon = require('./punto3');
-const formatPokemon = require('./punto4');
+// const infoPokemon = require('./punto3');
+// const formatPokemon = require('./punto4');
+const readFile = require('./punto6')
 
-async function main(numero) {
-  const info = await infoPokemon(numero);
+// async function main(numero) {
+//   const info = await infoPokemon(numero);
   
-  const {name, types} = info
-  formatPokemon(name, types)
-}
+//   const {name, types} = info
+//   formatPokemon(name, types)
+// }
 
-main(25)
+// main(25)
+
+readFile()
---
+++ quiz/punto6.js
@@ -0,0 +1,13 @@
+const fs =  require ('fs') ;
+
+function readFile(){
+  fs.readFile('datos.txt', 'utf-8', (err, data) => {
+    if(err) {
+      console.log('El archivo "datos.txt" no existe. ', err);
+    } else {
+      console.log(data);
+    }
+  })
+}
+
+module.exports = readFile;
